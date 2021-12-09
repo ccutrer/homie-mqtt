53a1b671fb06f1a2b3aa6b948f7ada0ed42e5579
Cody Cutrer <cody@cutrer.us>
2021-12-09 16:49:42 -0700

    add mqtt/homie file for easy requiring

--- lib/homie-mqtt.rb
+++ lib/homie-mqtt.rb
@@ -1,16 +1,3 @@
 # frozen_string_literal: true
 
-module MQTT
-  module Homie
-    class << self
-      def escape_id(id)
-        id.downcase.gsub(/[^a-z0-9\-]/, "-").sub(/^[^a-z0-9]+/, "")
-      end
-    end
-  end
-end
-
-require "mqtt/homie/base"
-require "mqtt/homie/device"
-require "mqtt/homie/node"
-require "mqtt/homie/property"
+require "mqtt/homie"
